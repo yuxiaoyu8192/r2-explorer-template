416211e1ba6fe1e32f34ca03ca205e203b065a68
yuxiaoyu8192 <103018181+yuxiaoyu8192@users.noreply.github.com>
2025-06-20 20:47:50 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import { R2Explorer } from "r2-explorer";
+import { R2Explorer } from 'r2-explorer';
 
-export default R2Explorer({
-  // Set to false to allow users to upload files
-  readonly: true,
-
-  // Learn more how to secure your R2 Explorer instance:
-  // https://r2explorer.com/getting-started/security/
-  // cfAccessTeamName: "my-team-name",
+export default R2Explorer({ readonly: false, basicAuth: [{
+    username: 'yuxiaoyu',
+    password: 'yuxiaoyu1024'
+  },]
 });
