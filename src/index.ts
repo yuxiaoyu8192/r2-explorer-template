4c2d20259096a970b2c08e1b1de35efaf9cb20f8
yuxiaoyu8192 <103018181+yuxiaoyu8192@users.noreply.github.com>
2025-06-25 00:18:10 +0800

    更新 index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { R2Explorer } from 'r2-explorer';
 
-export default R2Explorer({ readonly: false, basicAuth: [{
+export default R2Explorer({ readonly: false, basicAuth: {
     username: 'yuxiaoyu',
     password: 'yuxiaoyu1024'
-  },]
+  }
 });
